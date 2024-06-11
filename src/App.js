38cd425642cb8a2f017ff543aa69929d73c98fb0
bazhenak <bazhena.kutergina@gmail.com>
2024-06-11 21:36:45 +0300

    elements, methods Api

--- src/App.js
+++ src/App.js
@@ -2,32 +2,28 @@ import './index.css';
 import {Link, Route, Routes} from "react-router-dom";
 import './Home/Home.js'
 import Home from "./Home/Home";
-import Redirect from "./Redirect/Redirect";
-import About from "./About/About";
+import Contacts from "./Contacts/Contacts";
 
 
 export default function App() {
   return (
     <div className="App">
+        <h1>React приложение Api</h1>
         <div className='router-nav-container container'>
           <nav className='navigation-menu'>
               <div className='navigation-menu__container'>
                   <div>
-                      <Link to='/home'>Главная</Link>
+                      <Link to='/home'>Элементы с сервера</Link>
                   </div>
                   <div>
-                      <Link to='/contacts'>Redirect</Link>
-                  </div>
-                  <div>
-                      <Link to='/about'>О нас</Link>
+                      <Link to='/contacts'>Методы</Link>
                   </div>
               </div>
           </nav>
         </div>
         <Routes>
             <Route path='/home' element={<Home />} />
-            <Route path='/contacts' element={<Redirect />} />
-            <Route path='/about' element={<About />} />
+            <Route path='/contacts' element={<Contacts />} />
         </Routes>
     </div>
   );
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,41 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css'
-import './Clock.js'
-import Clock from "./Clock";
-import GetDate from "./Date";
-import StateComponent from "./Coordinates";
-import GetUserAgent from "./UserAgent";
 
 
 const Home = () => {
+
+    const [dataFromApi, setDataFromApi] = useState([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts/')
+            .then((result) => result.json())
+            .then((data) => setDataFromApi(data.splice(1, 10)));
+
+    }, [])
+
     return (
         <div className='home-page-container'>
-            <h2 className='home-page-container__head-text'>
-                Приветствуем Вас!
-            </h2>
-            <div>
-                <p>The time is:</p>
-                <Clock/>
-            </div>
-
-            <div>
-                <p>The date is:</p>
-                <GetDate/>
-            </div>
-
-            <div>
-                <p>Mouse coordinates (x) :</p>
-                <StateComponent/>
-            </div>
-
-            <div>
-                <p>Your user agent :</p>
-                <GetUserAgent/>
-            </div>
-
+            <p className='home-page-container__head-text'>Домашняя страница</p>
+            <p>Элементы с сервера:</p>
+            {dataFromApi.map((el, index) => {
+                return (
+                    <div key={index + el.id} className='home-page-container__item-container'>
+                        <h3 className='item-container__item-head'>{el.title}</h3>
+                        <div className='item-container__item-body'>{el.body}</div>
+                    </div>
+                );
+            })}
         </div>
     );
 };
 
-export default Home
+export default Home;
 
